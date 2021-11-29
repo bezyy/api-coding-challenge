ddef4777f37cbe1af20909e58cc3bcbfe22c60d2
olb26 <olb26@drexel.edu>
2021-11-29 18:19:01 -0500

    created GET method with streak tracking

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 import datetime
 
 app = Flask(__name__)
 api = Api(app)
+
+mood_post_args = reqparse.RequestParser()
 
 users = {}
 
 
 class User(Resource):
     def post(self, id, password):
-        users[id] = {"password": password, "mood": {}}
+        users[id] = {"password": password, "mood": {},
+                     "streak": 0, "next_update": "0000-00-00"}
         return users[id]
 
 
 class Mood(Resource):
-    def post(self, id, mood):
+    def post(self, id):
+        mood = request.form['mood']
         x = datetime.datetime.now()
+
+        # check if user submitted mood rating 1 day ago
+        if (str(x.date) == users[id]["next_update"]):
+            users[id]["streak"] += 1
+
+        next_update = x + datetime.timedelta(days=1)
+        users[id]["next_update"] = str(next_update.date())
         users[id]["mood"][str(x.date())] = mood
         return users[id]
 
     def get(self, id):
-        return users[id]["mood"] + "\n"
+        return users[id]["mood"]
 
 
 api.add_resource(User, "/user/<string:id>/<string:password>")
-api.add_resource(Mood, "/mood/<string:id>/<string:mood>")
+api.add_resource(Mood, "/mood/<string:id>")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -2,7 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
+# create user
 response = requests.post(BASE + "user/1/pass123")
 print(response.json())
-response = requests.post(BASE + "mood/1/3")
+
+# post mood
+response = requests.post(BASE + "mood/1", {"mood": 3})
+print(response.json())
+
+# get mood
+response = requests.get(BASE + "mood/1")
 print(response.json())
